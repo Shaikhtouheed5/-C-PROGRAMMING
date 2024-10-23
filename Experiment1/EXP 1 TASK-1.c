29bceaadd807e9fa64018f518652b747b0b4fa84
Shaikhtouheed5 <shaikhtouheed05@eng.rizvi.edu.in>
2024-10-24 03:17:30 +0530

    Add files via upload

---
+++ Experiment1/EXP 1 TASK-1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+    printf("NAME   :TOUHEED SHAIKH\n");
+    printf("AGE    :17\n");
+    printf("CLASS  :FE\n");
+    printf("DIV    :A\n");
+    printf("UIN    :241A067\n");
+       return 0;
+}
